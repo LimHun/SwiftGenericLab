d8a077cb415de994e2429fb9496ec16fa3cdbf74
임훈 <tunko@kyobobook.com>
2023-11-26 11:38:32 +0900

    Initial Commit

---
+++ SwiftGenericLab/SwiftGenericLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenericLabApp.swift
+//  SwiftGenericLab
+//
+//  Created by tunko on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenericLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
